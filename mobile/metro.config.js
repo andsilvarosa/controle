ae34fdff47c9a486a056691c236bf26c24bcf2c4
andsilvarosa <64783044+andsilvarosa@users.noreply.github.com>
2026-03-31 15:07:33 -0300

    chore: Remove unused and outdated assets
    This commit cleans up the project by removing several unused and outdated image assets, including cached icon files and a placeholder `assets/icon.png`. It also reverts changes to `metro.config.js` and `.env` that appear to be remnants of previous configurations or development steps.

--- mobile/metro.config.js
+++
@@ -1,9 +0,0 @@
-const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require("nativewind/metro");
-
-const baseConfig = getDefaultConfig(__dirname);
-const config = withNativeWind(baseConfig, { input: "./global.css" });
-
-config.transformerPath = require.resolve("./nativewind-transformer");
-
-module.exports = config;
